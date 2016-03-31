cdcc194f49a66e63f95852690e7adef529727b1e
vadimZ <01vadim10@gmail.com>
2016-03-31 16:05:11 +0300

    2nd commit

---
+++ EmployeeCRUD/Global.asax.cs
@@ -0,0 +1,11 @@
+﻿using System.Web.Http;
+using System.Web.Routing;
+
+namespace EssentialTools {
+    public class MvcApplication : System.Web.HttpApplication {
+        protected void Application_Start() {
+            
+
+        }
+    }
+}
